870d62bf7b7d0c549970d5dde42bcaafcc0b43e2
Libby Hamlin <bravolibby@gmail.com>
2020-03-26 22:55:15 -0700

    completed tutorial, but app is not working properly. City and temperature is not updating. See 25:20 on video for help.

--- app.js
+++ app.js
@@ -2,10 +2,44 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let temperatureTimezone = document.querySelector('.location-timezone');
+    
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
-            console.log(position);
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = 'http://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/636fefb277ae3e164b72291c8dfe071a/${lat},${long}`;
+            
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    const { temperature, summary, icon } = data.currently;
+                    //Set DOM Elements from API
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
+                    //Set Icons
+                    setIcons(icon, document.querySelector('icon'));
+
+
+                });
         });
+
+     
+    }
+
+    function setIcons(icon, iconId){
+        const skycons = new skycons({ color: 'white' });
+        const currentIcon = icon.replace(/-/g, '_').toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+
     }
 });

dfded9ddbecad9105f5643cd4d5d27eb36cf2a92
Libby Hamlin <bravolibby@gmail.com>
2020-03-26 21:48:31 -0700

    Completed basic/minimum CSS

---
+++ app.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            console.log(position);
+        });
+    }
+});

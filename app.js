0bd59767422c17d96c73c56331209f9d27326992
Libby Hamlin <bravolibby@gmail.com>
2020-03-28 22:34:13 -0700

    It works! Weather App updates with correct info

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ window.addEventListener('load', () => {
     let lat;
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
-    let temperatureTimezone = document.querySelector('.location-timezone');
+    let locationTimezone = document.querySelector('.location-timezone');
     
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
+            // console.log(position);
             long = position.coords.longitude;
             lat = position.coords.latitude;
 

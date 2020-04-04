4251dde9ab3144e4c97533a8f679992bd42ad526
Libby Hamlin <bravolibby@gmail.com>
2020-04-03 17:59:55 -0700

    Went back to troubleshoot why skycon icon was not showing up. Also, created function to change the degree to celsius/fahrenheit on click.

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ window.addEventListener('load', () => {
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
     let locationTimezone = document.querySelector('.location-timezone');
-    
+    let temperatureSection = document.querySelector('.temperature');
+    const temperatureSpan = document.querySelector('.temperature span');
+
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -26,18 +28,32 @@ window.addEventListener('load', () => {
                     temperatureDegree.textContent = temperature;
                     temperatureDescription.textContent = summary;
                     locationTimezone.textContent = data.timezone;
+
+                    //Formula for Celsius
+                    let celsius = (temperature - 32) * (5 / 9);
+
                     //Set Icons
-                    setIcons(icon, document.querySelector('icon'));
+                    setIcons(icon, document.querySelector('.icon'));
 
-
+                    //Change temperature to Celsius/Farenheit
+                    temperatureSection.addEventListener('click', () => {
+                        if (temperatureSpan.textContent === 'F') {
+                            temperatureSpan.textContent = 'C';
+                            temperatureDegree.textContent = Math.floor(celsius);
+                        
+                        } else {
+                            temperatureSpan.textContent = 'F';
+                            temperatureDegree.textContent = temperature;
+                        }
+                    });
                 });
         });
 
      
     }
 
-    function setIcons(icon, iconId){
-        const skycons = new skycons({ color: 'white' });
+    function setIcons(icon, iconID){
+        const skycons = new Skycons({ color: 'white' });
         const currentIcon = icon.replace(/-/g, '_').toUpperCase();
         skycons.play();
         return skycons.set(iconID, Skycons[currentIcon]);
